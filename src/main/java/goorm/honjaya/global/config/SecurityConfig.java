628cc4900d060f6096a80d1b3c7535ff09f47a3c
geon8692 <geon4735@gmail.com>
2024-05-29 00:58:56 +0900

    Fix: 시큐리티 설정 수정

--- src/main/java/goorm/honjaya/global/config/SecurityConfig.java
+++ src/main/java/goorm/honjaya/global/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/ws/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/oauth2/**")).permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 );
 
         http
