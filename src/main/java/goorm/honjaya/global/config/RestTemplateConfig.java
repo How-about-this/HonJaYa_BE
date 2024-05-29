f3e2f875b69d1a41179f63d8587714fe5635a24d
geon8692 <geon4735@gmail.com>
2024-05-29 11:59:33 +0900

    Feat: 배포환경 소셜로그인 위해 RestTemplate 설정 및 customLoadUser 직접 구현

---
+++ src/main/java/goorm/honjaya/global/config/RestTemplateConfig.java
@@ -0,0 +1,29 @@
+package goorm.honjaya.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Value("krmp-proxy.9rum.cc")
+    private String proxyHost;
+
+    @Value("3128")
+    private int proxyPort;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+        requestFactory.setProxy(proxy);
+
+        return new RestTemplate(requestFactory);
+    }
+}
