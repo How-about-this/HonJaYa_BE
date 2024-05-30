6c498f1dab053b43eb31bf5610d2a1be5043d4a1
geon8692 <geon4735@gmail.com>
2024-05-30 18:44:41 +0900

    Fix: CORS 경로 변경

--- src/main/java/goorm/honjaya/global/config/SecurityConfig.java
+++ src/main/java/goorm/honjaya/global/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Collections.singletonList("https://k8596b539d0daa.user-app.krampoline.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
