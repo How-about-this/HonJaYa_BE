4b88bbec29b1a6e53604a9682331bccd4ef9e525
gupark <ghp720@naver.com>
2024-06-17 17:00:12 +0900

    fix: 재구님 코드 롤백 / ChatMessage 형식 수정

--- src/main/java/goorm/honjaya/domain/chat/ChatMessage.java
+++ src/main/java/goorm/honjaya/domain/chat/ChatMessage.java
@@ -1,28 +1,26 @@
 package goorm.honjaya.domain.chat;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity // JPA 엔티티로 선언
 @Getter
 @Setter
 public class ChatMessage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private MessageType type;
     private String msg;
     private String sender;
     private String roomNum;
     private String isOwnMessage;
-    private String createdAt;
+    private String createAt;
 
     public enum MessageType {
         CHAT, JOIN, LEAVE
     }
+//    private String id;
+//    private String msg;
+//    private String sender;
+//    private String receiver;
+//    private String roomNum;
+//    private String isOwnMessage;
+//    private String createAt;
 }
--- src/main/java/goorm/honjaya/domain/chat/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package goorm.honjaya.domain.chat.repository;
-
-import goorm.honjaya.domain.chat.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-}
